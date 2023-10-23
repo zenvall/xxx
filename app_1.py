71ca16880674cd454ce5a89e7c85eb8056a329b0
Siddharth K <143085568+zenvall@users.noreply.github.com>
2023-10-23 13:22:46 +0530

    Add files via upload

---
+++ app_1.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import datetime
+import time
+import pandas as pd
+import base64
+
+# Define the event schedule
+event_schedule = [
+   
+    {"start": "07:00", "end": "08:00", "event": "Exercise"},
+    {"start": "08:00", "end": "09:00", "event": "Breakfast"},
+    {"start": "09:00", "end": "10:00", "event": "Work"},
+    {"start": "10:00", "end": "11:00", "event": "Work"},
+    {"start": "11:00", "end": "12:00", "event": "Work"},
+    {"start": "12:00", "end": "13:00", "event": "Work"},
+    {"start": "13:00", "end": "14:00", "event": "Lunch"},
+    {"start": "14:00", "end": "15:00", "event": "Work"},
+    {"start": "15:00", "end": "16:00", "event": "Work"},
+    {"start": "16:00", "end": "17:00", "event": "Work"},
+    {"start": "17:00", "end": "18:00", "event": "Rest"},
+    {"start": "18:00", "end": "19:00", "event": "Work"},
+    {"start": "19:00", "end": "20:00", "event": "Work"},
+    {"start": "20:00", "end": "21:00", "event": "Dinner"},
+    {"start": "21:00", "end": "22:00", "event": "Work"},
+    {"start": "22:00", "end": "23:00", "event": "Work"},
+    {"start": "23:00", "end": "00:00", "event": "Work on Website"},
+    {"start": "00:00", "end": "01:00", "event": "Youtube/book"},
+    {"start": "01:00", "end": "02:00", "event": "Twitch"},
+    {"start": "02:00", "end": "06:00", "event": "Sleep"},
+]
+
+def get_current_event(schedule, current_time):
+    # Function to find the current event
+    for event in schedule:
+        start_time = datetime.datetime.strptime(event["start"], "%H:%M").time()
+        end_time = datetime.datetime.strptime(event["end"], "%H:%M").time()
+
+        # Handle the case where the end time is "00:00"
+        if (start_time <= current_time < end_time) or (start_time <= current_time and end_time == datetime.time(0, 0)):
+            return event
+    return None
+
+def main():
+    st.title("Live Timetable")
+
+    current_time = datetime.datetime.now().time()
+    st.markdown(f"### Current Time: {current_time.strftime('%H:%M')}")
+
+    st.markdown("### Event Schedule")
+
+    df = pd.DataFrame(event_schedule)
+    st.dataframe(df)
+
+    current_event = get_current_event(event_schedule, current_time)
+    if current_event:
+        st.markdown(f"🔵 **Current Event:** {current_event['event']} ({current_event['start']} - {current_event['end']})")
+
+    # Add a button to download the event list as a CSV
+    if st.button("Download Event List (CSV)"):
+        csv_data = df.to_csv(index=False)
+        b64 = base64.b64encode(csv_data.encode()).decode()
+        href = f'<a href="data:file/csv;base64,{b64}" download="event_schedule.csv">Download Event List (CSV)</a>'
+        st.markdown(href, unsafe_allow_html=True)
+
+if __name__ == "__main__":
+    main()
